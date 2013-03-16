48768cd5a3354b805fe9a34cb435764d72f1d13a
Fernando Doglio <deleteman@gmail.com>
2013-03-16 10:09:16 -0300

    adding meta tags and analytics code

--- app/views/layout/layout.html.php
+++ app/views/layout/layout.html.php
@@ -2,6 +2,9 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<META NAME="Description" CONTENT="Find open source projects hosted on Github that you'd like to contribute to, or publish your own and find help here!">
+	<META NAME="Keywords" CONTENT="github, project, open source, pull request, contributing, programming">
+
 	<title>LFPR: Looking for Pull Requests</title>
 	<script type="text/javascript" src="/javascripts/jquery-1.8.2.min.js" ></script>
 	<script type="text/javascript" src="/javascripts/jquery-ui-1.8.23.custom.min.js" ></script>
@@ -17,6 +20,19 @@
 	<link rel="stylesheet" href="/stylesheets/ui-lightness/jquery-ui-1.8.23.custom.css" type="text/css" />
 </head>
 <body>
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39365402-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 	<div class="container">
 		
 		<div id="main-wrapper">
