45e2498845c8b6cfd7fa1a30cbcd9594a5f45107
Fernando Doglio <deleteman@gmail.com>
2013-08-16 22:30:08 +0100

    Removing login from config class

--- lib/ConfigClass.php
+++ lib/ConfigClass.php
@@ -7,14 +7,11 @@ class Config {
 	static private function load() {
 		$parser = new YAMLParser();
 		$config_path = ROOT_PATH . Config::CONFIG_FILE_PATH;
-		Makiavelo::info("trying to load config file: " . $config_path);
 		return $parser->parsePath($config_path);
 	}
 
 	static public function get($param_path) {
-		Makiavelo::info("Getting configuration parameter: " . $param_path);
 		$data = Config::load();
-		Makiavelo::info("Data loaded: " . print_r($data, true));
 		$keys = explode(".", $param_path);
 		$current = $data;
 		foreach($keys as $key) {
@@ -22,7 +19,6 @@ class Config {
 				$current = $current[$key];
 			}
 		}
-		Makiavelo::info("Value found: " . $current);
 		return $current;
 	}
 }
