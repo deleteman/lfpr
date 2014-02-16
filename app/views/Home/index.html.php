289f69b374f67a945f1862d21e3923b92affbbce
deleteman <deleteman@gmail.com>
2014-02-16 16:27:20 -0200

    Fixing syntax errors and minor change on button text

--- app/views/Home/index.html.php
+++ app/views/Home/index.html.php
@@ -1,93 +1,93 @@
 <div class="hero-unit">
-	<?php
-	if(user_logged_in()) { ?>
-	<div class="current-user">
-		<?=image_tag(current_user()->avatar_url)?>
-		<?=link_to(developer_show_path(current_user()), '<i class="icon-github-alt"></i> '.current_user()->name, array("class" => "user-link"))?>
-		/ <?=link_to(login_sign_out_path(), '<i class="icon-signout"></i> ', array("class" => "has-tooltip", "data-title" => "Sign out"))?>
-	</div>
-	<?php
-	} else { ?> 
-	<p class="pull-right">
-		<?=link_to(GithubAPI::login_url(), '<i class="icon-github" style="font-size:30px;" ></i> Github', array("class" => "has-tooltip btn btn-large btn-success", "data-title" => "Sign-in using Github"))?>
-	</p>
-	<?php } ?>
+  <?php
+  if(user_logged_in()) { ?>
+  <div class="current-user">
+    <?=image_tag(current_user()->avatar_url)?>
+    <?=link_to(developer_show_path(current_user()), '<i class="icon-github-alt"></i> '.current_user()->name, array("class" => "user-link"))?>
+    / <?=link_to(login_sign_out_path(), '<i class="icon-signout"></i> ', array("class" => "has-tooltip", "data-title" => "Sign out"))?>
+  </div>
+  <?php
+  } else { ?> 
+  <p class="pull-right">
+    <?=link_to(GithubAPI::login_url(), '<i class="icon-github" style="font-size:30px;" ></i> Github', array("class" => "has-tooltip btn btn-large btn-success", "data-title" => "Sign-in using Github"))?>
+  </p>
+  <?php } ?>
 <h3><img src="/img/LFPR-logo.png" /> </h3>
 <h1> Looking for Pull Requests!</h1>
 <p>
-	Use the site to find a project to contribute to, <em>OR</em> publish your projects and let the 
-	world know you're looking for contributors
+  Use the site to find a project to contribute to, <em>OR</em> publish your projects and let the 
+  world know you're looking for contributors
 </p>
-	<p class="pull-left">
-	<?=link_to(project_list_path(), "Find projects", array("class" => "btn btn-primary btn-large", "id" => "main-btn"))?>
+  <p class="pull-left">
+  <?=link_to(project_list_path(), "Find projects", array("class" => "btn btn-primary btn-large", "id" => "main-btn"))?>
 
-	<a class="palette palette-wet-asphalt promo-link" href="<?=home_contest_path()?>">
-		Win a cool T-Shirt and more! <br/>
-		Click here for more information...
-		<img src="/img/premios-iconos.png" alt="Awesome stuff!">
-	</a>
-	</p>
+  <a class="palette palette-wet-asphalt promo-link" href="<?=home_contest_path()?>">
+    Win a cool T-Shirt and more! <br/>
+    Click here for more information...
+    <img src="/img/premios-iconos.png" alt="Awesome stuff!">
+  </a>
+  </p>
 
-	<div class="well span4 pull-right">
-		<p>Subscribe to get weekly updates on the site and new repos published.</p>
-		<?=form_for($this->suscriptor) ?>
-		<?=email_field($this->suscriptor, "email", false, array("placeholder" => "your@email.com"));?>
-		<?=submit("Subscribe", array("class" => "btn btn-primary btn-large"))?>
-		<?=form_end_tag()?>
-	</div>
+  <div class="well span4 pull-right">
+    <p>Subscribe to get weekly updates on the site and new repos published.</p>
+    <?=form_for($this->suscriptor) ?>
+    <?=email_field($this->suscriptor, "email", false, array("placeholder" => "your@email.com"));?>
+    <?=submit("Subscribe", array("class" => "btn btn-primary btn-large"))?>
+    <?=form_end_tag()?>
+  </div>
 
-	<div class="clearfix"></div>
+  <div class="clearfix"></div>
 
 </div>
 <div class="row">
-	<div class="well span12" id="expanded-search-form">
-		<?php $this->renderView("../Project/_search_form")?>
-		<div class="clearfix"></div>
-	</div>
+  <div class="well span12" id="expanded-search-form">
+    <?php $this->renderView("../Project/_search_form")?>
+    <div class="clearfix"></div>
+  </div>
 </div>
-	<h3>Random projects</h3>
-	<div class="row latest-projects">
-		<?php
-		foreach($this->new_projects as $proj) { ?> 
-		<div class="span3 text-center">
-			<div class="project-spotlight">
-				<h3><?=link_to(project_show_path($proj), truncate_string($proj->name, 13), array("class" => "proj-name-link"))?></a></h3>
-				<div class="">
-					<ul class="simple-stats pull-left">
-						<li><span class="fui-menu-24"></span> <?=$proj->forks?> forks</li>
-						<li><span class="fui-heart-24"></span> <?=$proj->stars?> stars</li>
-					</ul>
-					<ul class="simple-stats pull-left">
-						<li><span class="fui-settings-24"></span> <?=$proj->language()?> </li>
-					<li ><span class="fui-man-24"></span> <?=link_to(developer_show_path($proj->owner()),
-										$proj->owner()->name,
-										array("class" => "dev-link", "data-title" => "See all projects by this user"))?></li>
-					</ul>	
-					<div class="clearfix"></div>
-				</div>
-				
-				<p><?=truncate_string($proj->description, 80)?></p>
-			
-				<?=link_to(project_show_path($proj), '<span class="fui-eye-24"></span> View Stats', array("class" => "btn btn-primary btn-large"))?>
+  <h3>Random projects</h3>
+  <div class="row latest-projects">
+    <?php
+    foreach($this->new_projects as $proj) { ?> 
+    <div class="span3 text-center">
+      <div class="project-spotlight">
+        <h3><?=link_to(project_show_path($proj), truncate_string($proj->name, 13), array("class" => "proj-name-link"))?></a></h3>
+        <div class="">
+          <ul class="simple-stats pull-left">
+            <li><span class="fui-menu-24"></span> <?=$proj->forks?> forks</li>
+            <li><span class="fui-heart-24"></span> <?=$proj->stars?> stars</li>
+          </ul>
+          <ul class="simple-stats pull-left">
+            <li><span class="fui-settings-24"></span> <?=$proj->language()?> </li>
+          <li ><span class="fui-man-24"></span> <?=link_to(developer_show_path($proj->owner()),
+                    $proj->owner()->name,
+                    array("class" => "dev-link", "data-title" => "See all projects by this user"))?></li>
+          </ul> 
+          <div class="clearfix"></div>
+        </div>
+        
+        <p><?=truncate_string($proj->description, 80)?></p>
+      
+        <?=link_to(project_show_path($proj), '<span class="fui-eye-24"></span> View Stats', array("class" => "btn btn-primary btn-large"))?>
 <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?=$proj->url()?>" data-text="Checkout this cool project on @Github! " data-via="lookingfor_pr" data-count="none">Tweet about it!</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-			</div>
-		</div>
-		<?php } ?>
-		<div class="span3 text-center">
-			<div class="project-spotlight" id="your-project">
-				<h3>Your project</h3>
-				<p>
-					<img src="/img/illustrations/clipboard.png">
-				</p>
-				<?php
-				if (user_logged_in()) {
-					link_to(developer_show_path(current_user()), '<i class="fui-plus-24"></i> Add Project'.current_user()->name, array("class" => "btn btn-primary btn-large"))
-				} else {
-					link_to(GithubAPI::login_url(), '<i class="icon-github" style="font-size:30px;" ></i> Github', array("class" => "has-tooltip btn btn-large btn-success", "data-title" => "Sign-in using Github"))
-				}
-				?>
-			
-			</div>
-		</div>
-	</div>
+      </div>
+    </div>
+    <?php } ?>
+    <div class="span3 text-center">
+      <div class="project-spotlight" id="your-project">
+        <h3>Your project</h3>
+        <p>
+          <img src="/img/illustrations/clipboard.png">
+        </p>
+        <?php
+        if (user_logged_in()) {
+          echo link_to(developer_show_path(current_user()), '<i class="fui-plus-24"></i> Publish', array("class" => "btn btn-primary btn-large"));
+        } else {
+          echo link_to(GithubAPI::login_url(), '<i class="icon-github" style="font-size:30px;" ></i> Github', array("class" => "has-tooltip btn btn-large btn-success", "data-title" => "Sign-in using Github"));
+        }
+        ?>
+      
+      </div>
+    </div>
+  </div>
